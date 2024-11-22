424668f53813c4a9d97fc2e8800a32c2d945498e
Jacob <jake.berner@icloud.com>
2024-11-21 22:38:31 -0500

    Completed Java101 Practice Problem 2 reverseArray

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+
 public class Main {
 public static void main(String[] args) {
     int[] arr = {4, 3, 2, 8, 3};
-    System.out.println(PracticeProblems.countOccurences(arr, 3));
+		System.out.println(PracticeProblems.reverseArray(arr));
+	}
+}
           
-}
-}
--- PracticeProblems.java
+++ PracticeProblems.java
@@ -1,11 +1,18 @@
 public class PracticeProblems {
-static int countOccurences(int[] arr, int n) {
-int count = 0;    
-for (int num : arr) 
-if (num == n) {
-    count++;
-}  
-return count; 
-}  
+    static int[] reverseArray(int[] arr) {
+      
+        int[] reversed = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
+        }
+        return reversed;  
+    }
+ 
 
-}
+    }
+
+
+
+
+    
+              
