b79adee06e9c1b1c03e63d29efebe01eda37ec8a
Jacob <jake.berner@icloud.com>
2024-11-24 18:29:43 -0500

    Completed Angle

--- java102/Point.java
+++ java102/Point.java
@@ -1,38 +1,20 @@
 package java102;
 
-	public class Point {
-		public final double x;
-		public final double y;
-	
-		// Constructor
-		public Point(double x, double y) {
-			this.x = x;
-			this.y = y;
-		}
-		public Point translateX(double t) {
-			return new Point(x + t, y);
-		}
-		public Point translateY(double t) {
-			return new Point(x, y + t);
-		}
-		public static Point centerOfMass(Point[] points){
-			if (points.length == 0){
-				return new Point (0, 0);
-			}
-			double sumX = 0;
-			double sumY = 0;
-
-			for (Point p : points) {
-				sumX += p.x;
-				sumY += p.y;
-			}
+		public class Point {
+			public final double x;
+			public final double y;
 		
-			double centerX = sumX / points.length;
-			double centerY = sumY / points.length;
-		
-			return new Point(centerX, centerY);
-		}
-
+			public Point(double x, double y) {
+				this.x = x;
+				this.y = y;
+			}
+			
+// Practice Problem Angle
+public double angle() {
+	double angleinrads = Math.atan2(y, x);
+	double angleindegrees = Math.toDegrees(angleinrads);
+	return angleindegrees;
+}
 		}
 	
 	
