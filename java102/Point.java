6212d54b7ec9a6fcf71fd1101704fe98941e6a76
Jacob <jake.berner@icloud.com>
2024-12-01 15:54:13 -0500

    Circle.java and Square.java added

--- java102/Point.java
+++ java102/Point.java
@@ -8,13 +8,53 @@ package java102;
 				this.x = x;
 				this.y = y;
 			}
+				
+				public Point translateX(double t) {
+					return new Point(x + t, y);
+				}
+				
+				public Point translateY(double t) {
+					return new Point(x, y + t);
+				}
+			
+				@Override
+				public String toString() {
+					return "(" + x + ", " + y + ")";
+				}
+			
+//Distance
+				public static double distance(Point p1, Point p2) {
+					return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+				}
+			
+			 
 			
 // Practice Problem Angle
-public double angle() {
-	double angleinrads = Math.atan2(y, x);
-	double angleindegrees = Math.toDegrees(angleinrads);
+			public double angle() {
+			double angleinrads = Math.atan2(y, x);
+			double angleindegrees = Math.toDegrees(angleinrads);
+
 	return angleindegrees;
-}
+			}
+	// Practice Problem centerofMass
+			public static Point centerOfMass(Point[] points){
+				if (points.length == 0){
+					return new Point (0, 0);
+				}
+				double sumX = 0;
+				double sumY = 0;
+	 
+	 
+				for (Point p : points) {
+					sumX += p.x;
+					sumY += p.y;
+				}
+		   
+				double centerX = sumX / points.length;
+				double centerY = sumY / points.length;
+		   
+				return new Point(centerX, centerY);
+			}
 		}
 		
 	
---
+++ java102/Square.java
@@ -0,0 +1,76 @@
+package java102;
+
+public class Square {
+    public final Point corner;
+    public final double sideLength;
+
+    /**
+    * @param corner The bottom left corner of the square
+    * @param sideLength The length of each side of the square
+    */
+    public Square(Point corner, double sideLength) {
+        this.corner = corner;
+        this.sideLength = sideLength;
+    }
+
+    // Method to calculate the area of the square
+    public double area() {
+        return sideLength * sideLength; // Area = sideLength^2
+    }
+
+    // Method to calculate the perimeter of the square
+    public double perimeter() {
+        return 4 * sideLength; // Perimeter = 4 * sideLength
+    }
+
+    /** 
+    * @return Whether point p is inside of the square.
+    */
+    public boolean isInside(Point p) {
+        double xDist = p.x - corner.x;
+        double yDist = p.y - corner.y;
+        return 0 < xDist && xDist < sideLength &&
+               0 < yDist && yDist < sideLength;
+    }
+
+    /** 
+    * @return Whether point p is part of/on the border of the square.
+    */
+    public boolean isOn(Point p) {
+        double xDist = p.x - corner.x;
+        double yDist = p.y - corner.y;
+        return (xDist == 0 || xDist == sideLength) && 0 <= yDist && yDist <= sideLength ||
+               (yDist == 0 || yDist == sideLength) && 0 <= xDist && xDist <= sideLength;
+    }
+
+    /** 
+    * @param x How much to translate the square by in the +x direction.
+    * @param y How much to translate the square by in the +y direction.
+    * @return The square that results from the translation.
+    */
+    public Square translate(double x, double y) {
+        return new Square(new Point(corner.x + x, corner.y + y), sideLength);
+    }
+
+    /** 
+    * @return The square that results from scaling the side length and maintaining the bottom left corner.
+    */
+    public Square scale(double k) {
+        return new Square(corner, sideLength * k); // Scale the side length by factor k
+    }
+
+    // Method to calculate the coordinates of all four corners of the square
+    public Point[] corners() {
+        Point[] corners = new Point[4];
+        corners[0] = corner; // Bottom-left corner
+        corners[1] = new Point(corner.x + sideLength, corner.y); // Bottom-right corner
+        corners[2] = new Point(corner.x + sideLength, corner.y + sideLength); // Top-right corner
+        corners[3] = new Point(corner.x, corner.y + sideLength); // Top-left corner
+        return corners;
+    }
+
+    @Override
+    public String toString() {
+        return "(corner: " + corner + "; side length: " + sideLength + ")";
+    }
+}
